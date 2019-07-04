04e865b74c0e18631f8b38ce1e4b60d204bf9fb7
donavon guyot <dguyot@dguyot-ltm2.internal.salesforce.com>
2019-07-04 12:04:06 -0700

    Add Kernl Update & Versioning Support

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
-<?php 
+<?php
 
-add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' ); 
+add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' );
 
-function my_enqueue_assets() { 
+function my_enqueue_assets() {
 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ); 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
-} 
+}
+
+require 'theme_update_check.php';
+$KernlUpdater = new ThemeUpdateChecker(
+    'divi-open-child',
+    'https://kernl.us/api/v1/theme-updates/5d1e4b5de340c33e182925a7/'
+);
